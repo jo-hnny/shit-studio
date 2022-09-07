8dba4c97caf1dc658dd7dabd3d3643196cfc598b
SwallowOfTheDay <1126862764@qq.com>
2022-09-07 11:29:08 +0800

    initCommit

--- src/modules/merge/pages/list/excelUpload/index.tsx
+++ src/modules/merge/pages/list/excelUpload/index.tsx
@@ -1,38 +1,122 @@
 import React, { useState } from 'react';
-import { Button, Modal, Upload, Form, Input } from 'antd';
+import { Button, Modal, Upload, Form, Input, Select } from 'antd';
 import { InboxOutlined } from '@ant-design/icons';
+import * as XLSX from 'xlsx';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  updateMainFileData,
+  updateSubFileData
+} from '../../../../../redux/merge';
 import { readXlslFileToJson } from '../../../../../helps';
 
+interface excelDataInterface {
+  fileName: string;
+  sheetJson: any[];
+}
+
+interface stateInterface {
+  mainFileData: excelDataInterface;
+  subFileData: excelDataInterface;
+}
+
 const { Dragger } = Upload;
-
+const { Option } = Select;
 export function ExcelUploadButton() {
+  const { mainFileData, subFileData } = useSelector(
+    (state: stateInterface) => state.merge
+  );
+  const dispatch = useDispatch();
   const [visible, setVisible] = React.useState(false);
-  const [fileName, setFileName] = useState('');
+  const [subVisible, setSubVisible] = React.useState(false);
+  const [fileType, setFileType] = React.useState('main');
   const [remark, setRemark] = useState('');
-  const [fileData, setFileData] = useState<Array<any> | null>(null);
-
+  const [mergeVisible, setMergeVisible] = useState(false);
   const handleUpload = async (file: File) => {
-    console.log(file);
-
     const data = await readXlslFileToJson(file);
-
-    setFileName(file?.name ?? '');
-
-    setFileData(data);
-
+    if (fileType === 'main') {
+      dispatch(
+        updateMainFileData({
+          fileName: file?.name ?? '',
+          sheetJson: data,
+        })
+      );
+    } else {
+      dispatch(
+        updateSubFileData({
+          fileName: file?.name ?? '',
+          sheetJson: data,
+        })
+      );
+    }
     return false;
+  };
+
+  const [mainColumns, setMainColumns] = useState('');
+  const [subColumns, setSubColumns] = useState('');
+  const [subMergeColumns, setSubMergeColumns] = useState('');
+  const [relation, setRelation] = useState(1);
+  const handleMerge = () => {
+    setMergeVisible(true);
+    const mainData = [];
+    mainFileData.sheetJson.forEach(e => {
+      const rowData = {
+        ...e,
+      };
+      subFileData.sheetJson.forEach(sub => {
+        // console.log(sub.__EMPTY_2, e.__EMPTY_2);
+        let flag = false;
+        if (relation === 1) {
+          flag = sub[subColumns] === e[mainColumns];
+        }
+        if (flag) {
+          rowData[subMergeColumns] = sub[subMergeColumns];
+        }
+      });
+      mainData.push(rowData);
+    });
+    const sheets = XLSX.utils.json_to_sheet(mainData);
+    const wopts = {
+      bookType: 'xlsx', // 要生成的文件类型
+      bookSST: false, // 是否生成Shared String Table，官方解释是，如果开启生成速度会下降，但在低版本IOS设备上有更好的兼容性
+      type: 'binary',
+    };
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, sheets, 'Sheet1');
+
+    XLSX.writeFile(wb, '合并数据.xlsx', wopts);
   };
 
   return (
     <>
       <Button
         type="primary"
-        style={{ marginBottom: '15px' }}
-        onClick={() => setVisible(true)}
+        style={{ marginBottom: '15px', marginRight: '15px' }}
+        onClick={() => {
+          setVisible(true);
+          setFileType('main');
+        }}
       >
-        上传
+        上传主表格
       </Button>
-
+      <Button
+        type="primary"
+        style={{ marginBottom: '15px', marginRight: '15px' }}
+        onClick={() => {
+          setSubVisible(true);
+          setFileType('sub');
+        }}
+      >
+        上传副表格
+      </Button>
+      <Button
+        type="primary"
+        style={{ marginBottom: '15px' }}
+        onClick={() => {
+          setMergeVisible(true);
+        }}
+      >
+        合并
+      </Button>
       <Modal
         title="上传表格"
         visible={visible}
@@ -43,7 +127,8 @@ export function ExcelUploadButton() {
             <Dragger
               accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
               beforeUpload={handleUpload}
-              disabled={fileData !== null}
+              // disabled={fileData !== null}
+              multiple
             >
               <p className="ant-upload-drag-icon">
                 <InboxOutlined />
@@ -53,14 +138,83 @@ export function ExcelUploadButton() {
           </Form.Item>
 
           <Form.Item label="文件名">
-            <Input
-              value={fileName}
-              onChange={e => setFileName(e?.target?.value ?? '')}
-            />
+            <Input value={mainFileData?.fileName} />
           </Form.Item>
 
           <Form.Item label="备注">
             <Input value={remark} onChange={e => setRemark(e?.target?.value)} />
+          </Form.Item>
+        </Form>
+      </Modal>
+      <Modal
+        title="上传副表格"
+        visible={subVisible}
+        onCancel={() => setSubVisible(false)}
+      >
+        <Form labelAlign="left" labelCol={{ span: 3 }}>
+          <Form.Item label="文件">
+            <Dragger
+              accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+              beforeUpload={handleUpload}
+              // disabled={fileData !== null}
+              multiple
+            >
+              <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p className="ant-upload-text">点击或者拖拽文件到这里进行上传</p>
+            </Dragger>
+          </Form.Item>
+
+          <Form.Item label="文件名">
+            <Input value={subFileData?.fileName} />
+          </Form.Item>
+
+          <Form.Item label="备注">
+            <Input value={remark} onChange={e => setRemark(e?.target?.value)} />
+          </Form.Item>
+        </Form>
+      </Modal>
+      <Modal
+        title="合并表格"
+        visible={mergeVisible}
+        okText="合并"
+        onOk={handleMerge}
+        onCancel={() => setMergeVisible(false)}
+      >
+        <Form labelAlign="left" labelCol={{ span: 10 }}>
+          <Form.Item label="主表格列">
+            <Select onChange={e => setMainColumns(e)}>
+              {Object.keys(mainFileData?.sheetJson[0] || {}).map(
+                (item: string) => (
+                  <Option value={item}>{item}</Option>
+                )
+              )}
+            </Select>
+          </Form.Item>
+          <Form.Item label="副表格列">
+            <Select onChange={e => setSubColumns(e)}>
+              {Object.keys(subFileData?.sheetJson[0] || {}).map(
+                (item: string) => (
+                  <Option value={item}>{item}</Option>
+                )
+              )}
+            </Select>
+          </Form.Item>
+          <Form.Item label="选择比较条件">
+            <Select onChange={e => setRelation(e)}>
+              <Option value={1}>等于</Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item label="满足条件时需要合并的数据列">
+            <Select onChange={e => setSubMergeColumns(e)}>
+              {Object.keys(subFileData?.sheetJson[0] || {}).map(
+                (item: string) => (
+                  <Option value={item}>{item}</Option>
+                )
+              )}
+            </Select>
           </Form.Item>
         </Form>
       </Modal>
--- src/modules/merge/pages/list/index.tsx
+++ src/modules/merge/pages/list/index.tsx
@@ -1,22 +1,61 @@
-import React from 'react';
-import { Table } from 'antd';
-import { listColumns } from './constants';
+import React, { useEffect, useState } from 'react';
+import { Table, Tabs } from 'antd';
+import { useSelector } from 'react-redux';
+
 import { ExcelUploadButton } from './excelUpload';
 
+const { TabPane } = Tabs;
+
 export function MergeListPage() {
-  const data = [
-    {
-      name: '热鼠标',
-      rowSize: 10,
-      remark: '阿水煎豆腐那块',
-    },
-  ];
+  const { mainFileData, subFileData } = useSelector(state => state.merge);
+
+  const [mainColumns, setMainColumns] = useState<any[]>([]);
+  const [subColumns, setSubColumns] = useState<any[]>([]);
+
+  const formatTableData = (data: any[]) => {
+    const keys = Object.keys(data[0]);
+    const columns = keys.map(v => ({
+      title: v,
+      dataIndex: v,
+      width: 200,
+    }));
+    return columns;
+  };
+
+  useEffect(() => {
+    if (mainFileData) {
+      setMainColumns(formatTableData(mainFileData.sheetJson));
+    }
+    if (subFileData) {
+      setSubColumns(formatTableData(subFileData.sheetJson));
+    }
+  }, [mainFileData, subFileData]);
+
+  // 主副表tab切换
 
   return (
     <>
       <ExcelUploadButton />
-
-      <Table columns={listColumns} dataSource={data} />
+      <Tabs defaultActiveKey="main">
+        {mainFileData && (
+          <TabPane tab={mainFileData.fileName} key="main">
+            <Table
+              columns={mainColumns}
+              dataSource={mainFileData.sheetJson}
+              rowKey="id"
+            />
+          </TabPane>
+        )}
+        {subFileData && (
+          <TabPane tab={subFileData.fileName} key="sub">
+            <Table
+              columns={subColumns}
+              dataSource={subFileData.sheetJson}
+              rowKey="id"
+            />
+          </TabPane>
+        )}
+      </Tabs>
     </>
   );
 }
